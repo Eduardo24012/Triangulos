43b18d8faa9e72d5ebc6b6b617a8abaca0349fbf
Eduardo24012 <eduardohdlc2405@gmail.com>
2025-05-21 20:07:33 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            clsTriangulo miTriangulo = new clsTriangulo();
+
+            int opcion = 1;
+            do
+            {
+                switch(opcion)
+                {
+                    case 1:
+
+                        Console.WriteLine("TIPOS DE TRIANGULOS");
+                        Console.WriteLine("registra el lado A del triangulo");
+                        miTriangulo.setLadoA(float.Parse(Console.ReadLine()));
+                        Console.WriteLine("Registra el lado B de triangulo");
+                        miTriangulo.setLadoB(float.Parse(Console.ReadLine()));
+                        Console.WriteLine("Registra el lado C del triangulo");
+                        miTriangulo.setLadoC(float.Parse(Console.ReadLine()));
+                        Console.WriteLine("El perimetro del triangulo es: " + miTriangulo.Perimetro() + " m");
+                        Console.WriteLine("El area del triangulo es: " + miTriangulo.Area() + " m2");
+                        Console.WriteLine("EL TIPO DE TRIÁNGULO ES UN: " + miTriangulo.TipoTriangulo());
+                        Console.WriteLine();
+                        break;
+                        
+                }
+
+            }while(opcion != 2);
+        }
+    }
+}
---
+++ clsTriangulo.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangulo
+{
+    internal class clsTriangulo
+    {
+        //Atributos
+        protected float LadoA;
+        protected float LadoB;
+        protected float LadoC;
+
+        //Constructor
+        public clsTriangulo()
+        {
+            LadoA = 0;
+            LadoB = 0;
+            LadoC = 0;
+        }
+        //Propiedades
+        public float getLadoA()
+        {
+            return LadoA;
+        }
+        public void setLadoA(float valor)
+        {
+            LadoA = valor;
+        }
+        public float getLadoB()
+        {
+            return LadoB;
+        }
+        public void setLadoB(float valor)
+        {
+            LadoB = valor;
+        }
+        public float getLadoC()
+        {
+            return LadoC;
+        }
+        public void setLadoC(float valor)
+        {
+            LadoC = valor;
+        }
+        //Metodos
+        public float Perimetro()
+        {
+            float res = 0;
+            res= LadoA+LadoB+LadoC;
+            return res;
+        }
+        public float Area()
+        {
+            float res = 0, result = 0;
+            float S;
+            S = Perimetro()/2;
+            result = S * (S - LadoA) * (S - LadoB) * (S - LadoC);
+            res = (float)Math.Sqrt(result);
+            return res;
+
+        }
+        public string TipoTriangulo()
+        {
+            if (LadoA == LadoB && LadoB == LadoC)
+            {
+                return "TRIÁNGULO EQUILÁTERO";
+            }
+            else if (LadoA == LadoB || LadoA == LadoC || LadoB == LadoC)
+            {
+                return "TRIÁNGULO ISÓSCELES";
+            }
+            else
+                return "TRIÁNGULO ESCALENO";
+        }
+    }
+}
